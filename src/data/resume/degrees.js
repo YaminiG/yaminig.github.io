dffed90939cb99223723621e71299801718ee637
Yamini Gaur <yaminigaur@Yaminis-MBP.attlocal.net>
2021-06-02 23:57:28 -0400

    adding full cgpa

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -4,14 +4,14 @@ const degrees = [
     degree: 'M.S. Computer Engineering',
     link: 'https://ece.vt.edu/grad/masters',
     year: 2019,
-    gpa: 3.92,
+    gpa: '3.92/10',
   },
   {
     school: 'Vellore Institute of Technology',
     degree: 'B.S. Electronics and Communication Engineering',
     link: 'https://vit.ac.in/',
     year: 2017,
-    gpa: 8.72,
+    gpa: '8.72/10',
   },
 ];
 
